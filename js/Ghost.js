f279d736bdc08dce77b5d73b064782689f9f4ab3
Pippin Barr <pippin.barr@gmail.com>
2020-04-23 16:51:54 -0400

    GHOST: Cursor is interruptable

--- js/Ghost.js
+++ js/Ghost.js
@@ -20,19 +20,34 @@ class Ghost extends BaseChess {
 
     setTimeout(() => {
       this.ghostClick('e2');
-    }, 2000)
+    }, 2000);
 
+    // $(document).on('click', () => {
+    //   this.cursor.stop();
+    // });
   }
 
-  ghostClick(square) {
+  ghostClick(square, callback) {
     let $square = $(`.square-${square}`);
+    let startTop = this.cursor.offset().top;
+    let startLeft = this.cursor.offset().left;
     let top = $square.offset().top + $square.height() / 2;
     let left = $square.offset().left + $square.width() / 2;
+    let dx = top - startTop;
+    let dy = left - startLeft;
+    let distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    let speed = 0.5;
+    console.log(distance);
     this.cursor.animate({
       top: `${top}px`,
       left: `${left}px`
-    }, 1000, () => {
-      $square.trigger('click');
+    }, distance / speed, () => {
+      this.cursor.animate({
+        top: `+=0px`
+      }, 2000, () => {
+        $square.trigger('click');
+        callback();
+      });
     });
   }
 
